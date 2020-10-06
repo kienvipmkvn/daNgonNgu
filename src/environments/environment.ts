a2d8b8b5ac5a8e4cf10f487c6c23cfbad9d6919e
kienvipmkvn <59178525+kienvipmkvn@users.noreply.github.com>
2020-10-06 15:32:08 +0700

    .

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: false,
   api:{
-    loginRoute: 'http://192.168.1.48:9001/api/v2/authentication/login',
+    loginRoute: 'http://192.168.1.48:8000/api/v2/authentication/login',
 
-    languageApi:'http://192.168.1.48:9004/api/v1/language', // Link đến API ngôn ngữ
-    cultureApi:'http://192.168.1.48:9004/api/v1/culture', // Link đến API ngôn ngữ
-    appTypeApi:'http://192.168.1.48:9004/api/v1/language/appType', // Link đến API ngôn ngữ
-    userApi:'http://192.168.1.48:9004/api/v1/language/user', // Link đến API ngôn ngữ
+    languageApi:'http://192.168.1.48:8000/api/v1/language', // Link đến API ngôn ngữ
+    cultureApi:'http://192.168.1.48:8000/api/v1/culture', // Link đến API ngôn ngữ
+    appTypeApi:'http://192.168.1.48:8000/api/v1/language/appType', // Link đến API ngôn ngữ
+    userApi:'http://192.168.1.48:8000/api/v1/language/user', // Link đến API ngôn ngữ
   },
   errorList: ["Cannot insert duplicate key in object"],
   regex: /^[^<>]*$/  
